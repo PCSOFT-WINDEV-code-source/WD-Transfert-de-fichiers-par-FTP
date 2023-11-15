bab717f5a3b7e79e163e6d541154567bb2a5f61f
PCSOFT-WINDEV <appli@pcsoft.fr>
2023-11-15 12:43:09 +0100

    Initial commit

---
+++ exinfos.ex
@@ -0,0 +1,9 @@
+[INFOS]
+gaf=0
+title=WD Transfert de fichiers par FTP
+image=description.gif
+description=description.rtf
+source=1
+preview=preview.png
+[OBJECT]
+WD Transfert de fichiers par FTP.WDP=1
